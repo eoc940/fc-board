78572c438f50ecc2f60c2a5b7607cc34e6113d08
Hyunggeun Kim <hgk0000@naver.com>
2024-08-29 10:51:48 +0900

    feat: 테스트 주석 커밋

--- src/main/kotlin/com/fastcampus/fcboard/FcBoardApplication.kt
+++ src/main/kotlin/com/fastcampus/fcboard/FcBoardApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class FcBoardApplication
-
+// 테스트 주석
 fun main(args: Array<String>) {
     runApplication<FcBoardApplication>(*args)
 }
